4818be9dac00a64d0bff787ad2bac0d0e4d20130
ibendrup <ibendrup@gmail.com>
2011-10-08 23:30:37 +0400

    new icons
    Signed-off-by: ibendrup <ibendrup@gmail.com>

--- RemoteControl/gen/remote/control/R.java
+++ RemoteControl/gen/remote/control/R.java
@@ -15,6 +15,9 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int next=0x7f020001;
+        public static final int prev=0x7f020002;
+        public static final int start=0x7f020003;
     }
     public static final class id {
         public static final int account_entry=0x7f070004;
